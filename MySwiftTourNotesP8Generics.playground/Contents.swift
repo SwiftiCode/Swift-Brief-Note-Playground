88430052f21fce465321ffd94efd66093f0cbdfe
SwiftiCode <swifticode@gmail.com>
2016-06-01 23:13:45 +0800

    final

--- MySwiftTourNotesP8Generics.playground/Contents.swift
+++ MySwiftTourNotesP8Generics.playground/Contents.swift
@@ -1,11 +1,9 @@
 //: ## Generics
 //:
-//: Generics are those with unknown data type
-//: We can create genertic forms of function, class, methods, structure and enumeration
-//:
-//: The purpose of creating generic form of function, methods or class is for developer to apply different data type to the function, method or class.
-//:
-//: To define generic, we need to postfix a generic `<T>` after the function name where T is the placeholder name for generic.
+//: + Generics are those data type that is unknown.
+//: + We can create generics in the forms of function, class, methods, structure and enumeration
+//: + The purpose of creating generic form of function, methods or class is for developer to apply different data type to the function, method or class.
+//: + To define generic, we need to apply a postfix with a `<T>` after the function name where T is the placeholder name for generic.
 func print10Times<T> (x: T) {
     for _ in 1...10 {
         print(x)
@@ -19,19 +17,20 @@ print10Times(genB)
 let genC = 12.2
 print10Times(genC)
 
-//: We can use `where` after the type name to specify a set of condition or requirement for the data type we called type constraint.
-//: We can demand the generic data type to be subsclass from a specific class or to implement certain protocol.
+//: + We can use `where` after the type name to specify a set of condition or requirement for those data type we can use. We called such condition type constraint.
+//: + We can demand the generic data type to be subsclass from a specific class or to implement certain protocol.
 //: 
-//: In the following example, we compare if the first input is larger than second input. We use T as generic type. However, without the statement "where T: Comparable". The system will have error as we cannot compare generic unless we constraint the generic type to comparable
+//: + In the following example, we will compare if the first input is larger than second input. We use T as generic type. 
+//: + However, without the statement `where T: Comparable`. The system will give error as we cannot compare generic unless we constraint the generic type to comparable
 func isLarger<T where T: Comparable >(first: T, second: T) -> Bool {
     return first > second
 }
 
 isLarger(100, second: 20)
-isLarger(12.0212, second: 124.25)
+isLarger(19972.0212, second: 124.25)
 
-//: The can include other constraint such as T must conform to a protocol or T and U must be the same.
+//: We can include other constraint such as we can insist that `T` must conform to a protocol or `T` and `U` (second generic type) must be the same.
 
-//: ##End
+//: ## The End
 
 
