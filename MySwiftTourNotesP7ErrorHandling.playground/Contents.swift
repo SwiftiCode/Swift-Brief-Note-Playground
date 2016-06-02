257494fc695b86a8648c6a7d199c466d70bbb518
SwiftiCode <swifticode@gmail.com>
2016-06-02 08:38:20 +0800

    Final check and commit for Part 7 - 2 Jun 2016

--- MySwiftTourNotesP7ErrorHandling.playground/Contents.swift
+++ MySwiftTourNotesP7ErrorHandling.playground/Contents.swift
@@ -110,7 +110,7 @@ if let canBuy = try? executeBuy(transaction2) {
 
 user1.funds
 
-//: + Please note that when we use `try?` method, if there is any error, the error will de discarded so it is no use in using `do-catch`.
+//: + Please note that when we use `try?` method, if there is any error, the error will be discarded so it is no use in using `do-catch`.
 //:
 //: + We can also use keyword `defer` to write a block of code that is executed regardless if the function throws an error.
 func executeBuy2(transDetail: buyStuff) throws -> Bool {
